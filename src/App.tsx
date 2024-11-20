1ff51b884d8324d7a1a0cfa9b46c4da2106df8db
konstuanotop <konstantin-1997@mail.ru>
2024-11-21 04:15:28 +0500

    Replacing the component state hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react';
 import styles from './App.module.scss'
 import Counter from './components/Counter/Counter';
 
 function App() {
 
-  const [count, setCount] = useState(0)
-
   return (
     <div className={styles.App}>
-      <Counter count={count} setCount={setCount} />
+      <Counter />
     </div>
   );
 }
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,24 +1,69 @@
-import { Dispatch, SetStateAction } from 'react';
+import { useReducer } from 'react'
 import styles from './Counter.module.scss'
 
-interface CounterProps {
+interface State {
     count: number;
-    setCount: Dispatch<SetStateAction<number>>
 }
 
-const Counter: React.FC<CounterProps> = ({ count, setCount }) => {
+interface IncrementAction {
+    type: 'INCREMENT'
+}
+
+interface DecrementAction {
+    type: 'DECREMENT'
+}
+
+interface ResetAction {
+    type: 'RESET'
+}
+
+type Action = IncrementAction | DecrementAction | ResetAction;
+
+const initialState: State = { count: 0 };
+
+
+const reducer = (state: State, action: Action): State => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                ...state,
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                ...state,
+                count: state.count - 1
+            };
+        case 'RESET':
+            return initialState;
+        default:
+            return state;
+    }
+}
+
+
+const Counter: React.FC = () => {
+
+    const [state, dispatch] = useReducer<React.Reducer<State, Action>>(reducer, initialState);
+
     return (
         <div className={styles.Counter}>
             <h1 className={styles.header}>Счетчик:</h1>
-            <div className={styles.count}>{count}</div>
+            <div className={styles.count}>{state.count}</div>
             <button
                 className={styles.btn_blue}
-                onClick={() => setCount((count) => count - 1)}
+                onClick={() => dispatch({ type: 'DECREMENT' })}
             >- Минус</button>
             <button
                 className={styles.btn_red}
-                onClick={() => setCount((count) => count + 1)}
+                onClick={() => dispatch({ type: 'INCREMENT' })}
             >Плюс +</button>
+            <div>
+                <button
+                    className={styles.btn_reset}
+                    onClick={() => dispatch({ type: 'RESET' })}
+                >Reset</button>
+            </div>
         </div>
     )
 }
